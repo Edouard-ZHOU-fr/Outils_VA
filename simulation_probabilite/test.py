b98ceab557ace971e415acacb7508f9603413e7d
Hongyu <hongyu@example.com>
2024-10-09 16:24:35 +0200

    mise en forme le code

--- simulation_probabilite/test.py
+++ simulation_probabilite/test.py
@@ -12,6 +12,9 @@ print(rand_choice)
 rand_float = random.random()
 print(rand_float) 
 
+
+
+
 # une chiffre float aléatoire entre A et B
 rand_float = random.uniform(2.5, 5.5)
 print(rand_float) 
@@ -22,13 +25,21 @@ rand_int = random.randint(1, 10)
 print(rand_int) 
 
 
+
 # choisir k éléments dans la liste
 seq = ['apple', 'banana', 'orange', 'pear', 'grape']
 rand_sample = random.sample(seq, 3)
 print(rand_sample)
 
+
+
 # génerer une chiffre aléa dans la range((start, stop, step))
 rand_range = random.randrange(0, 10, 2)
 
+
+
 # géneration une chiffre qui obei la gausse  gauss(mu, sigma) dont mu为均值，sigma为标准差
 rand_gauss = random.gauss(0, 1)
+
+
+
